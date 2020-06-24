376b83edd8972f6624bc0e1fdf08bcc9c34c39b2
Edu <e.aguirre@itecriocuarto.org.ar>
2020-06-23 21:25:42 -0300

    Subiendo los archivos a github

---
+++ js/miprogramacion.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("#contnido").load("home.html");
+    $("#preguntasFrecuentes").click(function(){
+        $("#contenido").load("preguntas_frecuentes.html");
+    });
+    $("#producto").click(function(){
+        $("#contenido").load("producto.html");
+    });
+    $("#combo1").click(function(){
+        $("#contenido").load("venta.html");
+    });
+    $("#combo2").click(function(){
+        $("#contenido").load("venta_b.html");
+    });
+    $("#terminos").click(function(){
+        $("#contenido").load("terminos_condiciones.html");
+    });
+    $("#home").click(function(){
+        $("#contenido").load("home.html");
+    });
+});
+
